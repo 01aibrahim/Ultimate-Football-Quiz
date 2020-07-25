3fc46276aa9b3b5b32bfd7cfe230e8501038fbaa
Abdulrahman Ibrahim <51540733+01aibrahim@users.noreply.github.com>
2020-07-25 19:17:38 +0100

    Initial Commit

---
+++ Ultimate Football Quiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ultimate Football Quiz
+//
+//  Created by Abdulrahman ibrahim on 25/07/2020.
+//  Copyright © 2020 Abdulrahman ibrahim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
